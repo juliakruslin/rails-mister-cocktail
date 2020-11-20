1ab76196a815b9d4dd2a274369958aa434e37991
Julia Kruslin <julia.kruslin@gmail.com>
2020-11-20 16:25:19 +0100

    pics added

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+Cocktail.destroy_all
 
 p Cocktail.create(name: "The Julia")
 p Cocktail.create(name: "The best")
